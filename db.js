979d374268a7f1b4443448e66049322759867dd2
SamirDas80983 <147432866+SamirDas80983@users.noreply.github.com>
2026-03-18 13:08:34 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 // Define mongodb connect url
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels'
+//const mongoURL = 'mongodb://127.0.0.1:27017/hotels' //local URL and it will connect to local database
+//const mongoURL = process.env.MONGODB_URL_LOCAL// it is also local URL and it will connect to local database
+const mongoURL = process.env.MONGODB_URL;
 
 // set up MongoDB connection
 mongoose.connect(mongoURL)
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();//it is an instance of express or all express data will be store in app and we do the work by using app
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // • bodyParser.json() automatically parses the JSON data from the request body and converts it into a JavaScript object, which is then stored in the req.body.
+const PORT = process.env.PORT || 3000; // we do this to use the port like whenever we put the node on online then in online they will give a port || use local port 3000 if port not available
 
 const Person = require('./models/Person');
 
@@ -24,6 +26,6 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/person',personRoutes)
 app.use('/menu',menuItemRoutes)
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log('listening on port 3000');
 })
